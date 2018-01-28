48fe83f9fa29abe2199e673a4097570de308dfb2
sai sri ram <ssram4298@gmail.com>
2018-01-28 21:56:50 +0530

    fixed pylint problems and network disconnection errors

--- youtube.py
+++ youtube.py
@@ -1,6 +1,5 @@
 """ Youtube Downloader """
 
-# from time import sleep
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -9,73 +8,80 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-search = input("enter your search: ")
-r = requests.get("https://www.youtube.com/results?search_query=" + search)
-
-soup = BeautifulSoup(r.content, "html.parser")
-
-results = soup.find_all("h3", {"class": "yt-lockup-title"})
-
-COUNT = 1
-VIDEOS = []
-MY_LIST = []
-
-for items in results:
-    MY_LIST.append(items.a.text)
-    VIDEOS.append(items.a["href"])
-
-print("List Of Videos Found")
-print("=================================")
-for i in MY_LIST:
-    print(str(COUNT) + ')' + i)
-    COUNT += 1
-
-print("================================")
-choice = int(input("Enter Your Choice: "))
-
-output = "https://youtube.com" + VIDEOS[choice-1]
-
-driver = webdriver.Chrome()
-driver.get("http://en.savefrom.net/")
+SEARCH = input("enter your search: ")
 
 try:
-    submit = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "sf_submit"))
-    )
-    link_box = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "sf_url"))
-    )
-    link_box.send_keys(output)
-    print("entered address")
-    submit.click()
-    print("submit button clicked")
+    R = requests.get("https://www.youtube.com/results?search_query=" + SEARCH)
+
+    SOUP = BeautifulSoup(R.content, "html.parser")
+
+    RESULTS = SOUP.find_all("h3", {"class": "yt-lockup-title"})
+
+    COUNT = 1
+    VIDEOS = []
+    MY_LIST = []
+
+    for items in RESULTS:
+        MY_LIST.append(items.a.text)
+        VIDEOS.append(items.a["href"])
+
+    print("List Of Videos Found")
+    print("=================================")
+    for i in MY_LIST:
+        print(str(COUNT) + ')' + i)
+        COUNT += 1
+
+    print("================================")
+    CHOICE = int(input("Enter Your Choice: "))
+
+    OUTPUT = "https://youtube.com" + VIDEOS[CHOICE-1]
+
+    DRIVER = webdriver.Chrome()
+    DRIVER.get("http://en.savefrom.net/")
 
     try:
-        drop_box = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "drop-down-box"))
+        SUBMIT = WebDriverWait(DRIVER, 10).until(
+            EC.presence_of_element_located((By.ID, "sf_submit"))
         )
+        LINK_BOX = WebDriverWait(DRIVER, 10).until(
+            EC.presence_of_element_located((By.ID, "sf_url"))
+        )
+        LINK_BOX.send_keys(OUTPUT)
+        print("entered address")
+        SUBMIT.click()
+        print("submit button clicked")
 
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-        quality = soup.find("div", {"class":"drop-down-box"})
-        itms = quality.find_all("a")
-        lst = []
-        vid = []
-        for it in itms:
-            lst.append(it.text)
-            vid.append(it["href"])
+        try:
+            DROP_BOX = WebDriverWait(DRIVER, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "drop-down-box"))
+            )
 
-        ct = 1
-        for i in lst:
-            print(str(ct)+")"+i)
-            ct += 1
+            SOUP = BeautifulSoup(DRIVER.page_source, "html.parser")
+            QUALITY = SOUP.find("div", {"class":"drop-down-box"})
+            ITEMS = QUALITY.find_all("a")
+            QUALITY_LIST = []
+            VIDEO_LINKS = []
+            for it in ITEMS:
+                QUALITY_LIST.append(it.text)
+                VIDEO_LINKS.append(it["href"])
 
-        print("===============================")
-        choice = int(input("Enter your choice: "))
-        print("===============================")
-        print("Your video is being downloaded...............")
-        driver.get(vid[choice-1])
+            COUNT2 = 1
+            for i in QUALITY_LIST:
+                print(str(COUNT2) +")"+i)
+                COUNT2 += 1
 
+            print("===============================")
+            CHOICE = int(input("Enter your choice: "))
+            print("===============================")
+            print("Your video is being downloaded...............")
+            DRIVER.get(VIDEO_LINKS[CHOICE - 1])
+
+        finally:
+            print("done!")
     finally:
-        print("done!")
-finally:
-    print("submit button clicked")
+        print("submit button clicked")
+
+except R:
+    print("**************************************")
+    print("Can't connect to youtube")
+    print("**************************************")
