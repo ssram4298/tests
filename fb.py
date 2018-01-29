8d05e93c58c6d5c8bc2520e28f288bc98a346eb5
Sai Sri Ram Ampapurapu <ssram4298@gmail.com>
2018-01-29 19:31:33 +0530

    removed unnecessary code
    changed fb.py to only login

--- fb.py
+++ fb.py
@@ -1,13 +1,10 @@
-""" facebook login and logout  """
+""" facebook login """
 
 from time import sleep
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 
-USER = '8019886716'
-PASS = 'srasta4007@'
+USER = ''     #add your user name
+PASS = ''     #add your password
 
 DRIVER = webdriver.Chrome()
 DRIVER.get('https://www.facebook.com/')
@@ -24,29 +21,6 @@ print("Password entered")
 LOGIN_BTN = DRIVER.find_element_by_id('loginbutton')
 LOGIN_BTN.click()
 
-sleep(10)
-LOGOUT1 = DRIVER.find_element_by_css_selector("#userNavigationLabel")
-LOGOUT1.click()
-sleep(10)
-
-# try:
-#     LOGOUT1 = WebDriverWait(DRIVER, 10).until(
-#         EC.presence_of_element_located((By.ID, "userNavigationLabel")),
-#         EC.element_to_be_clickable((By.ID, "userNavigationLabel"))
-#     )
-#     print("button found")
-#     LOGOUT1.click()
-#     try:
-#         LOGOUT2 = WebDriverWait(DRIVER, 20).until(
-#             EC.presence_of_element_located((By.CSS_SELECTOR,
-#               "._w0d[action='https://www.facebook.com/logout.php']"))
-#         )
-#         LOGOUT2.submit()
-#     finally:
-#         print("logout button found")
-# finally:
-#     print("element clicked")
-
-input('Press anything to quit bitch')
+input('Press anything to quit')
 DRIVER.quit()
 print("Finished")
