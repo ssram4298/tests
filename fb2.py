54dfe7dab65c61efc8a650be4061402d3c8d259d
Sai Sri Ram Ampapurapu <ssram4298@gmail.com>
2018-01-29 19:35:49 +0530

    updated fb2.py as both login and logout program
    still need to work on logout part of fb2.py

--- fb2.py
+++ fb2.py
@@ -1,34 +1,43 @@
-""" facebook login and logout  """
+""" facebook login and logout """
+ 
+ from time import sleep
+ from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+ 
+USER = ''     #add your user name
+PASS = ''     #add your password
+ 
+ DRIVER = webdriver.Chrome()
+ DRIVER.get('https://www.facebook.com/')
+  
+ LOGIN_BTN = DRIVER.find_element_by_id('loginbutton')
+ LOGIN_BTN.click()
+ 
+sleep(10)
+LOGOUT1 = DRIVER.find_element_by_css_selector("#userNavigationLabel")
+LOGOUT1.click()
+sleep(10)
 
-from time import sleep
-from selenium import webdriver
+# try:
+#     LOGOUT1 = WebDriverWait(DRIVER, 10).until(
+#         EC.presence_of_element_located((By.ID, "userNavigationLabel")),
+#         EC.element_to_be_clickable((By.ID, "userNavigationLabel"))
+#     )
+#     print("button found")
+#     LOGOUT1.click()
+#     try:
+#         LOGOUT2 = WebDriverWait(DRIVER, 20).until(
+#             EC.presence_of_element_located((By.CSS_SELECTOR,
+#               "._w0d[action='https://www.facebook.com/logout.php']"))
+#         )
+#         LOGOUT2.submit()
+#     finally:
+#         print("logout button found")
+# finally:
+#     print("element clicked")
 
-USER = '8019886716'
-PASS = 'srasta4007@'
-
-DRIVER = webdriver.Chrome()
-DRIVER.get('https://www.facebook.com/')
-print("Opened facebook")
-sleep(1)
-
-USERNAME = DRIVER.find_element_by_id('email')
-USERNAME.send_keys(USER)
-print("Email Id entered")
-sleep(1)
-
-PASSWORD = DRIVER.find_element_by_id('pass')
-PASSWORD.send_keys(PASS)
-print("Password entered")
-
-LOGIN_BTN = DRIVER.find_element_by_id('loginbutton')
-LOGIN_BTN.click()
-
-# logout1 = DRIVER.find_element_by_css_selector("#pageLoginAnchor")
-# logout1.click()
-# sleep(20)
-# logout2 = DRIVER.find_element_by_css_selector("").submit()
-print("Done")
-
-input('Press anything to quit bitch')
+input('Press anything to quit')
 DRIVER.quit()
 print("Finished")
